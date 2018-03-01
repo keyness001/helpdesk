3f1e40cadb5ce7a86a47aaadf3a78934530e8ee5
51203104 <51203104@hcmut.edu.vn>
2018-03-02 00:35:09 +0700

    update

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleID: '772710770951-dm2f4q64am08ltsgoemo8b3i110tka4m.apps.googleusercontent.com',
+  googleSecret: 'fboHOMGRq-oeOtyIBq8b09Vg',
+  mongoID: 'mongodb://keyness001:Sang746916@ds127994.mlab.com:27994/stp-react',
+  secret: 'reactfullstackbeginer'
+}
